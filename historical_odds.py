72e8cf062df22847d261e093c9fc36907db7da52
timr894 <timr894@gmail.com>
2024-07-23 22:04:57 -0600

    files split and working

---
+++ historical_odds.py
@@ -0,0 +1,49 @@
+# historical_odds.py
+import requests
+import datetime
+
+SDQL_USERNAME = 'TimRoss'
+SDQL_TOKEN = '3b88dcbtr97bb8e89b74r'
+
+def get_sdql_data(sport_key, date):
+    sdql_query = f"date,team,site,runs,total,line@date={date.strftime('%Y%m%d')}"
+    sdql_url = f"https://s3.sportsdatabase.com/{sport_key}/query"
+
+    headers = {
+        'user': SDQL_USERNAME,
+        'token': SDQL_TOKEN
+    }
+
+    data = {
+        'sdql': sdql_query
+    }
+
+    response = requests.get(sdql_url, headers=headers, params=data)
+
+    print(f"Request URL: {sdql_url}")
+    print(f"Request Headers: {headers}")
+    print(f"Request Data: {data}")
+    print(f"Response Status Code: {response.status_code}")
+    print(f"Response Headers: {response.headers}")
+    print(f"Response Content: {response.content}")
+
+    if response.status_code != 200:
+        print(f"Unexpected Content-Type: {response.headers['Content-Type']}")
+        print(f"Response content: {response.content.decode('utf-8')}")
+        return None
+
+    try:
+        result = response.json()
+        print(f"Response JSON: {result}")
+
+        if result.get('headers') and result.get('groups'):
+            headers = result['headers']
+            rows = result['groups'][0]['columns']
+            formatted_result = [dict(zip(headers, row)) for row in zip(*rows)]
+        else:
+            formatted_result = None
+
+        return formatted_result
+    except ValueError as e:
+        print(f"Error parsing JSON response: {str(e)}")
+        return None
---
+++ odds_api.py
@@ -0,0 +1,35 @@
+# odds_api.py
+import requests
+import datetime
+
+api_key = '0888cd14d7c715c2ee32735fba20b332'
+
+def get_odds_data(sport_key, date):
+    try:
+        date_str = date.strftime('%Y-%m-%d')
+        scores_url = f"https://api.the-odds-api.com/v4/sports/{sport_key}/scores/?apiKey={api_key}&date={date_str}&dateFormat=iso"
+        odds_url = f"https://api.the-odds-api.com/v4/sports/{sport_key}/odds/?apiKey={api_key}&bookmakers=bovada&markets=h2h,spreads,totals&oddsFormat=american"
+
+        scores_response = requests.get(scores_url)
+        odds_response = requests.get(odds_url)
+
+        scores_response.raise_for_status()
+        odds_response.raise_for_status()
+
+        scores = scores_response.json()
+        odds = odds_response.json()
+
+        return scores, odds
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching odds data: {str(e)}")
+        return None, None
+
+def get_sports():
+    try:
+        api_url = f"https://api.the-odds-api.com/v4/sports/?apiKey={api_key}"
+        response = requests.get(api_url)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print('Error fetching sports:', str(e))
+        return None

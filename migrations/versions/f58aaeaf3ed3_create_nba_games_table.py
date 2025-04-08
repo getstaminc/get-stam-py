4b944320d58792a5eef615085203147118727e44
stephanieagillen <stephanieagillen@gmail.com>
2025-04-08 12:21:30 -0600

    update file

--- migrations/versions/f58aaeaf3ed3_create_nba_games_table.py
+++ migrations/versions/f58aaeaf3ed3_create_nba_games_table.py
@@ -1,26 +1,37 @@
-"""Create nba_games table
-
-Revision ID: f58aaeaf3ed3
-Revises: 8d200c5054dd
-Create Date: 2025-04-08 12:13:23.301237
-
-"""
-from typing import Sequence, Union
+"""Create nba_games table"""
 
 from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
-revision: str = 'f58aaeaf3ed3'
-down_revision: Union[str, None] = '8d200c5054dd'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+# Revision identifiers, used by Alembic.
+revision = 'f58aaeaf3ed3'
+down_revision = '8d200c5054dd'
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        'nba_games',
+        sa.Column('game_id', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('game_date', sa.Date(), nullable=False),
+        sa.Column('game_site', sa.String(length=10), nullable=False),
+        sa.Column('home_team_id', sa.Integer(), sa.ForeignKey('teams.team_id'), nullable=False),
+        sa.Column('away_team_id', sa.Integer(), sa.ForeignKey('teams.team_id'), nullable=False),
+        sa.Column('home_points', sa.Integer(), nullable=False),
+        sa.Column('away_points', sa.Integer(), nullable=False),
+        sa.Column('total_points', sa.Float(), nullable=True),
+        sa.Column('total_margin', sa.Float(), nullable=True),
+        sa.Column('home_line', sa.Float(), nullable=True),
+        sa.Column('away_line', sa.Float(), nullable=True),
+        sa.Column('quarter_scores', sa.JSON(), nullable=True),
+        sa.Column('home_halftime_points', sa.Integer(), nullable=True),
+        sa.Column('away_halftime_points', sa.Integer(), nullable=True),
+        sa.Column('sdql_game_id', sa.Integer(), unique=True, nullable=True),
+        sa.PrimaryKeyConstraint('game_id')
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table('nba_games')

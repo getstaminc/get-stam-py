3c3981a540977e9cc7f53a6977c007eaaab09b15
stephanieagillen <stephanieagillen@gmail.com>
2025-05-10 15:06:46 -0600

    add nba_games table

---
+++ migrations/versions/a5240ce0ea20_new_nba_migration_table.py
@@ -0,0 +1,49 @@
+"""new nba migration table
+
+Revision ID: a5240ce0ea20
+Revises: 7db73c9b7216
+Create Date: 2025-05-06 21:52:58.744914
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import func
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'a5240ce0ea20'
+down_revision: Union[str, None] = '7db73c9b7216'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'nba_games',
+        sa.Column('game_id', sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column('game_date', sa.Date(), nullable=False),
+        sa.Column('game_site', sa.String(length=10), nullable=False),
+        sa.Column('home_team_id', sa.Integer(), sa.ForeignKey('teams.team_id'), nullable=False),
+        sa.Column('away_team_id', sa.Integer(), sa.ForeignKey('teams.team_id'), nullable=False),
+        sa.Column('home_team_name', sa.String(length=100), nullable=False),  # Pretty name for home team
+        sa.Column('away_team_name', sa.String(length=100), nullable=False),  # Pretty name for away team
+        sa.Column('home_points', sa.Integer(), nullable=False),
+        sa.Column('away_points', sa.Integer(), nullable=False),
+        sa.Column('total_points', sa.Float(), nullable=True),
+        sa.Column('total_margin', sa.Float(), nullable=True),
+        sa.Column('home_line', sa.Float(), nullable=True),
+        sa.Column('away_line', sa.Float(), nullable=True),
+        sa.Column('quarter_scores', sa.JSON(), nullable=True),
+        sa.Column('home_halftime_points', sa.Integer(), nullable=True),
+        sa.Column('away_halftime_points', sa.Integer(), nullable=True),
+        sa.Column('sdql_game_id', sa.Integer(), unique=True, nullable=True),
+        sa.Column('created_date', sa.DateTime(timezone=True), server_default=func.now(), nullable=False),  # Auto-populated on creation
+        sa.Column('modified_date', sa.DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False),  # Auto-updated on modification
+        sa.PrimaryKeyConstraint('game_id')
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('nba_games')

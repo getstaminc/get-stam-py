ae539217f25a881b5f485bdba3424b105d1a0c96
stephanieagillen <stephanieagillen@gmail.com>
2025-09-16 21:03:36 -0600

    remove origin

--- app.py
+++ app.py
@@ -57,8 +57,6 @@ port = 5000
 # Specify allowed origins
 CORS(app, origins=[
     "http://localhost:3000",
-    "http://localhost:5000",
-    "http://127.0.0.1:3000",
     "http://127.0.0.1:5000",
     "https://www.getstam.com"
 ])

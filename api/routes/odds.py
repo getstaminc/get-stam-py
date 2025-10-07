b692ac6d7de5ac95abf5ccf7622c1713040a741c
stephanieagillen <stephanieagillen@gmail.com>
2025-10-07 15:36:00 -0600

    add odds cache

--- api/routes/odds.py
+++ api/routes/odds.py
@@ -1,13 +1,15 @@
+# Add cache import
+from cache import cache
+
 import os
 from flask import Blueprint, request, jsonify, abort
 from dotenv import load_dotenv
-
 from ..services.game_service import GameService
+
+odds_bp = Blueprint('odds', __name__)
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
-odds_bp = Blueprint('odds', __name__)
 
 @odds_bp.before_request
 def check_api_key():
@@ -23,6 +25,7 @@ def check_api_key():
 # =============================================================================
 
 @odds_bp.route('/api/odds/<sport_key>', methods=['GET'])
+@cache.cached(timeout=120, query_string=True)
 def get_odds_for_sport(sport_key):
     """Get all games with odds for a sport, optionally filtered by date"""
     current_date = request.args.get('date', None)
@@ -35,6 +38,7 @@ def get_odds_for_sport(sport_key):
     return jsonify(result)
 
 @odds_bp.route('/api/odds/<sport_key>/<game_id>', methods=['GET'])
+@cache.cached(timeout=120, query_string=True)
 def get_single_game_odds(sport_key, game_id):
     """Get odds details for a single game by game_id"""
     result, error = GameService.get_single_game(sport_key, game_id)

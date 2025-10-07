79b5b21f2e5cedc46cd7e18628bfed978e334543
stephanieagillen <stephanieagillen@gmail.com>
2025-10-06 20:30:43 -0600

    make nhl in season

--- getstam-react/src/configs/sportsConfig.ts
+++ getstam-react/src/configs/sportsConfig.ts
@@ -7,7 +7,7 @@ export interface Sport {
 export const sports: Sport[] = [
   { name: "MLB", path: "/mlb", inSeason: true },
   { name: "NBA", path: "/nba", inSeason: false },
-  { name: "NHL", path: "/nhl", inSeason: false },
+  { name: "NHL", path: "/nhl", inSeason: true },
   { name: "NFL", path: "/nfl", inSeason: true },
   { name: "NCAAF", path: "/ncaaf", inSeason: true },
   { name: "EPL", path: "/epl", inSeason: true },

ad43766cba8bb7cfd0a2179d51bd1976556e2e16
stephanieagillen <stephanieagillen@gmail.com>
2025-12-05 11:47:29 -0700

    remove unused code

--- betting_guide.py
+++
@@ -1,8 +0,0 @@
-# betting_guide.py
-from flask import Blueprint, render_template
-
-betting_guide = Blueprint('betting_guide', __name__)
-
-@betting_guide.route('/betting-guide')
-def guide():
-    return render_template('betting_guide.html')
